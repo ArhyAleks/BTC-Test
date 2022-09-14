bc38baaaebe107231af5cd4313ce8898b769b8e9
ArkhyAleks <al.arkhypov@gmail.com>
2022-09-14 18:40:37 +0300

    Prettier

--- BitCoin/js/index.js
+++ BitCoin/js/index.js
@@ -1,10 +1,8 @@
 function countBit() {
-    let num1 = Number(document.getElementById("value_1").value);
-    let num2 = Number(document.getElementById("value_2").value);
-    if (Number.value == "") {
-      document.getElementById("outputbit").innerHTML = 0;
-    } else {
-      document.getElementById("outputbit").innerHTML = num2 / num1;
-    }
+  let num1 = Number(document.getElementById("value_1").value);
+  let num2 = Number(document.getElementById("value_2").value);
+   
+  document.getElementById("outputbit").innerHTML = num2 / num1;
+    
 }
 
